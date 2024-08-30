d084c57f0f41f254cc84a09c642c2f89482e2869
isaltosm <isaltosm@unemi.edu.ec>
2024-08-29 23:18:08 -0500

    add first class

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        userName: 'IvanSaltos85',
+        name: 'Iván Saltos Medina',
+        isFollowing: true
+    },
+    {
+        userName: 'monifer88',
+        name: 'Mónica Ruíz Lárraga',
+        isFollowing: true
+    },
+    {
+        userName: 'ScottMoneto',
+        name: 'Scott Saltos Ruíz',
+        isFollowing: true
+    },
+    {
+        userName: 'wendyCitaLove',
+        name: 'Wendy Peñafiel',
+        isFollowing: false
+    }
+]
+
+export function App () {
+    const fresita = {userName: 'fresita', name: 'Fresia Verónica'}
+    const [name, setName] = useState('machitoBuu')
+    const [userName, setUserName] = useState('interesada')
+
+    console.log("Rendered with name:", name)
+
+    return (
+        <section className='App'>
+            {
+                users.map(({ userName, name, isFollowing}) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                        name={name}
+                    />
+                ))
+            }
+        </section>
+    )
+
+    // return (
+    //     <section className='App'>
+    //         <TwitterFollowCard userName="IvanSaltos85" name="Iván Saltos Medina"/>
+    //         {/*Comentario*/}
+    //         {/* <TwitterFollowCard userName="scottferoz" name="Scott"/>
+    //         <TwitterFollowCard userName="monifer88" name="Mónica Ruíz"/>
+    //         <TwitterFollowCard userName={userName} name={name}/>
+    //         <TwitterFollowCard userName="wendycitaamor" name="Wendy Peñafiel" initialIsFollowing={true} />
+    //         <TwitterFollowCard {...fresita}/> */}
+
+    //         {/* <button onClick={() => setName('Gatito michu michu')}>
+    //             Cambiar Nombre
+    //         </button>
+    //         <button onClick={() => setUserName('Mishelle Loca')}>
+    //             Cambiar User
+    //         </button> */}
+    //     </section>
+    // )
+}
---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,13 @@
+// Duplicar linea SHIFT + ALT + FLECHA ABAJO
+// Comentario CTRL + }
+// Borrar linea SHIT + SUPR 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import './index.css' 
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <App/>
+)
